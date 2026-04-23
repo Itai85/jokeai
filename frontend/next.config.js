5ab8ebd6fb825b204f209e486f3a494e1e7d2e7d
Itai85 <itai.shultz@hotmail.com>
2026-04-23 22:13:49 +1000

    Add frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.jokeai.com', 'lh3.googleusercontent.com'],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+};
+
+module.exports = withPWA(nextConfig);
